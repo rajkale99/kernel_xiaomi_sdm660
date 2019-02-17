c3ac293387f5b2bcd2025e2892f91514ee078981
kdrag0n <dragon@khronodragon.com>
2019-02-16 20:58:04 -0800

    cpu_input_boost: Make frame boost timeout configurable
    This will make it easier to tune this in the future while also allowing
    users more freedom.
    
    Suggested-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/cpu_input_boost.h
+++ include/linux/cpu_input_boost.h
@@ -20,6 +20,7 @@ void cpu_input_boost_kick(void);
 void cpu_input_boost_kick_max(unsigned int duration_ms);
 void cpu_input_boost_kick_wake(void);
 void cpu_input_boost_kick_general(unsigned int duration_ms);
+bool cpu_input_boost_should_boost_frame(void);
 #else
 static inline void cpu_input_boost_kick(void)
 {
@@ -30,6 +31,10 @@ static inline void cpu_input_boost_kick_max(unsigned int duration_ms)
 static inline void cpu_input_boost_kick_wake(void)
 static inline void cpu_input_boost_kick_general(unsigned int duration_ms)
 {
+}
+static inline bool cpu_input_boost_should_boost_frame(void)
+{
+	return false;
 }
 #endif
 
