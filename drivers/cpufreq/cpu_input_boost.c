830d9e7ea5fdb5219dcc1510669d6ffe2e0154a5
kdrag0n <dragon@khronodragon.com>
2019-02-24 21:53:26 -0800

    cpu_input_boost: Apply a negative SchedTune boost on display off
    Negative SchedTune boosting can reduce power consumption when the
    display is off, as performance is not critical in such scenarios. This
    can be configured as desired in Kconfig and sysfs. Boost can be as low
    as -100, which somewhat resembles the powersave governor according to
    LWN: https://lwn.net/Articles/704859/
    
    Test: /dev/stune/schedtune.boost reads 0 when display is on and -50 when
    display is off
    Test: adjusting suspend_stune_boost parameter works
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -44,10 +44,12 @@ module_param(frame_boost_timeout, uint, 0644);
 static __read_mostly int input_stune_boost = CONFIG_INPUT_STUNE_BOOST;
 static __read_mostly int max_stune_boost = CONFIG_MAX_STUNE_BOOST;
 static __read_mostly int general_stune_boost = CONFIG_GENERAL_STUNE_BOOST;
+static __read_mostly int suspend_stune_boost = CONFIG_SUSPEND_STUNE_BOOST;
 
 module_param_named(dynamic_stune_boost, input_stune_boost, int, 0644);
 module_param(max_stune_boost, int, 0644);
 module_param(general_stune_boost, int, 0644);
+module_param(suspend_stune_boost, int, 0644);
 #endif
 
 /* Available bits for boost_drv state */
@@ -78,6 +80,7 @@ struct boost_drv {
 	int max_stune_slot;
 	bool general_stune_active;
 	int general_stune_slot;
+	int root_stune_default;
 };
 
 static struct boost_drv *boost_drv_g;
@@ -380,7 +383,10 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	if (*blank == FB_BLANK_UNBLANK) {
 		set_boost_bit(b, SCREEN_AWAKE);
 		__cpu_input_boost_kick_max(b, CONFIG_WAKE_BOOST_DURATION_MS);
+		set_stune_boost("/", b->root_stune_default, NULL);
 	} else {
+		set_stune_boost("/", suspend_stune_boost,
+				&b->root_stune_default);
 		clear_boost_bit(b, SCREEN_AWAKE);
 		unboost_all_cpus(b);
 	}
@@ -500,6 +506,7 @@ static int __init cpu_input_boost_init(void)
 	INIT_WORK(&b->general_boost, general_boost_worker);
 	INIT_DELAYED_WORK(&b->general_unboost, general_unboost_worker);
 	atomic_set(&b->state, 0);
+	b->root_stune_default = 0;
 
 	b->cpu_notif.notifier_call = cpu_notifier_cb;
 	b->cpu_notif.priority = INT_MAX - 2;
