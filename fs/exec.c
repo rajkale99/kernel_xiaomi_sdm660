72c1c80f0759c8ed6256ac86950e37cacfaf349f
rebenok90x <ctwoon@suicide.today>
2020-02-23 19:02:24 +0000

    fs: drop remnant parts of su_exec

--- fs/exec.c
+++ fs/exec.c
@@ -1638,7 +1638,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	would_dump(bprm, bprm->file);
 
 	/* exec_binprm can release file and it may be freed */
-	is_su = d_is_su(file->f_path.dentry);
+//	is_su = d_is_su(file->f_path.dentry);
 
 	retval = exec_binprm(bprm);
 	if (retval < 0)
